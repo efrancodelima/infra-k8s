5ba079619f344d2223e18c18a1f3478089bbef67
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2024-11-18 18:33:28 -0300

    Divisão do arquivo main.tf

---
+++ aws-eks-cluster.tf
@@ -0,0 +1,17 @@
+resource "aws_eks_cluster" "tf_eks_cluster" {
+  name     = "lanchonete-eks-cluster"
+  role_arn = aws_iam_role.tf_eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.tf_public_subnet[0].id,
+      aws_subnet.tf_public_subnet[1].id,
+      aws_subnet.tf_private_subnet[0].id,
+      aws_subnet.tf_private_subnet[1].id
+    ]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_policy
+  ]
+}
---
+++ aws-variables.tf
@@ -0,0 +1,15 @@
+# Recebe as variáveis do action
+variable "aws_region" {
+  description = "AWS region"
+  type = string
+}
+
+variable "aws_zone_1" {
+  description = "AWS AZ 1"
+  type = string
+}
+
+variable "aws_zone_2" {
+  description = "AWS AZ 2"
+  type = string
+}
