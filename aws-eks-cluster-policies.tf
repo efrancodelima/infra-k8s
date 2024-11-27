f8721bf5f1db20a0222228c4e73906c1439947ac
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2024-11-26 22:16:53 -0300

    Otimização do script de importação

--- aws-eks-cluster-policies.tf
+++ aws-eks-cluster-policies.tf
@@ -1,21 +1,21 @@
-resource "aws_iam_role_policy_attachment" "tf_cluster_policy_eks" {
+resource "aws_iam_role_policy_attachment" "tf_cluster_policy_1" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.tf_eks_cluster_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "tf_cluster_policy_rds" {
+resource "aws_iam_role_policy_attachment" "tf_cluster_policy_2" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
   role       = aws_iam_role.tf_eks_cluster_role.name
 }
 
 # O Cloud Watch é o coletor de métricas
-resource "aws_iam_role_policy_attachment" "tf_cluster_policy_cloud_watch" {
+resource "aws_iam_role_policy_attachment" "tf_cluster_policy_3" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
   role       = aws_iam_role.tf_eks_cluster_role.name
 }
 
 # Cada nó dentro do EKS Cluster é uma instância EC2
-resource "aws_iam_role_policy_attachment" "tf_cluster_policy_ec2" {
+resource "aws_iam_role_policy_attachment" "tf_cluster_policy_4" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
   role       = aws_iam_role.tf_eks_cluster_role.name
 }
--- resource_import.sh
+++ resource_import.sh
@@ -5,7 +5,8 @@ import_resource() {
   local resource_name=$2
   local resource_id=$3
 
-  terraform import "$resource_type.$resource_name" "$resource_id" || true
+  terraform import "$resource_type.$resource_name" "$resource_id" \
+  || echo "Erro ao importar o recurso, continuando..."
 }
 
 # Constantes
@@ -14,16 +15,21 @@ EKS_NODE_GROUP_NAME="lanchonete-eks-node-group"
 SECURITY_GROUP_ID="sg-07766e32f9a1be753"
 
 CLUSTER_ROLE_NAME="lanchonete-eks-cluster-role"
-CLUSTER_POLICY_NAME_1="AmazonEKSClusterPolicy"
-CLUSTER_POLICY_NAME_2="AmazonRDSFullAccess"
-CLUSTER_POLICY_NAME_3="CloudWatchFullAccess"
-CLUSTER_POLICY_NAME_4="AmazonEC2FullAccess"
-
 NG_ROLE_NAME="lanchonete-eks-node-group-role"
-NG_POLICY_NAME_1="AmazonEKSWorkerNodePolicy"
-NG_POLICY_NAME_2="AmazonRDSFullAccess"
-NG_POLICY_NAME_3="AmazonEKS_CNI_Policy"
-NG_POLICY_NAME_4="AmazonEC2ContainerRegistryReadOnly"
+
+CLUSTER_POLICIES=(
+  "AmazonEKSClusterPolicy"
+  "AmazonRDSFullAccess"
+  "CloudWatchFullAccess"
+  "AmazonEC2FullAccess"
+)
+
+NG_POLICIES=(
+  "AmazonEKSWorkerNodePolicy"
+  "AmazonRDSFullAccess"
+  "AmazonEKS_CNI_Policy"
+  "AmazonEC2ContainerRegistryReadOnly"
+)
 
 # Importa o cluster EKS
 import_resource "aws_eks_cluster" "tf_eks_cluster" "${EKS_CLUSTER_NAME}"
@@ -37,31 +43,19 @@ import_resource "aws_security_group" "tf_eks_security_group" "${SECURITY_GROUP_I
 # Importa a role do cluster
 import_resource "aws_iam_role" "tf_eks_cluster_role" "$CLUSTER_ROLE_NAME"
 
-# Importa os policy attachment da role do cluster
-import_resource "aws_iam_role_policy_attachment" "tf_cluster_policy_eks" \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${CLUSTER_POLICY_NAME_1}"
-
-import_resource "aws_iam_role_policy_attachment" "tf_cluster_policy_rds" \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${CLUSTER_POLICY_NAME_2}"
-
-import_resource "aws_iam_role_policy_attachment" "tf_cluster_policy_cloud_watch" \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${CLUSTER_POLICY_NAME_3}"
-
-import_resource "aws_iam_role_policy_attachment" "tf_cluster_policy_ec2" \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${CLUSTER_POLICY_NAME_4}"
-
 # Importa a role do node group
 import_resource "aws_iam_role" "tf_eks_node_group_role" "$NG_ROLE_NAME"
 
-# Importa os policy attachment da role do node group
-import_resource "aws_iam_role_policy_attachment" "tf_ng_policy_eks" \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${NG_POLICY_NAME_1}"
+# Importa a role do cluster e as políticas associadas
+import_resource "aws_iam_role" "tf_eks_cluster_role" "$CLUSTER_ROLE_NAME"
+for i in "${!CLUSTER_POLICIES[@]}"; do
+  import_resource "aws_iam_role_policy_attachment" "tf_cluster_policy_$((i+1))" \
+  "${CLUSTER_ROLE_NAME}/arn:aws:iam::aws:policy/${CLUSTER_POLICIES[$i]}"
+done
 
-import_resource "aws_iam_role_policy_attachment" "tf_ng_policy_rds" \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${NG_POLICY_NAME_2}"
-
-import_resource "aws_iam_role_policy_attachment" "tf_ng_policy_eks_cni" \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${NG_POLICY_NAME_3}"
-
-import_resource "aws_iam_role_policy_attachment" "tf_ng_policy_ec2" \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${NG_POLICY_NAME_4}"
+# Importa a role do node group e as políticas associadas
+import_resource "aws_iam_role" "tf_eks_node_group_role" "$NG_ROLE_NAME"
+for i in "${!NG_POLICIES[@]}"; do
+  import_resource "aws_iam_role_policy_attachment" "tf_ng_policy_$((i+1))" \
+  "${NG_ROLE_NAME}/arn:aws:iam::aws:policy/${NG_POLICIES[$i]}"
+done
