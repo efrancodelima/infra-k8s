79e5a0aab13010a6b327734cc1bf88408f8f9d9b
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2024-11-26 21:47:37 -0300

    Ajuste nas secrets

--- aws-variables.tf
+++ aws-variables.tf
@@ -1,16 +1,19 @@
 variable "aws_region" {
   description = "AWS region"
   type = string
+  default = "us-east-1"
 }
 
 variable "aws_zone_1" {
   description = "AWS AZ 1"
   type = string
+  default = "us-east-1a"
 }
 
 variable "aws_zone_2" {
   description = "AWS AZ 2"
   type = string
+  default = "us-east-1b"
 }
 
 variable "vpc_id" {
--- resource_import.sh
+++ resource_import.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-import_resource() {
-  local resource_type=$1
-  local resource_name=$2
-  local resource_id=$3
-
-  terraform import \
-  -var="aws_region=$AWS_REGION" \
-  -var="aws_zone_1=$AWS_ZONE_1" \
-  -var="aws_zone_2=$AWS_ZONE_2" \
-  "$resource_type.$resource_name" "$resource_id" || true
-}
-
 # Constantes
 EKS_CLUSTER_NAME="lanchonete-eks-cluster"
 EKS_NODE_GROUP_NAME="lanchonete-eks-node-group"
@@ -30,42 +18,42 @@ NG_POLICY_NAME_3="AmazonEKS_CNI_Policy"
 NG_POLICY_NAME_4="AmazonEC2ContainerRegistryReadOnly"
 
 # Importa o cluster EKS
-import_resource "aws_eks_cluster" "tf_eks_cluster" "${EKS_CLUSTER_NAME}"
+terraform import aws_eks_cluster.tf_eks_cluster "${EKS_CLUSTER_NAME}" | true
 
 # Importa o node group
-import_resource "aws_eks_node_group" "tf_eks_node_group" "${EKS_CLUSTER_NAME}:${EKS_NODE_GROUP_NAME}"
+terraform import aws_eks_node_group.tf_eks_node_group "${EKS_CLUSTER_NAME}:${EKS_NODE_GROUP_NAME}" | true
 
 # Importa o security group
-import_resource "aws_security_group" "tf_eks_security_group" "${SECURITY_GROUP_ID}"
+terraform import aws_security_group.tf_eks_security_group "${SECURITY_GROUP_ID}" | true
 
 # Importa a role do cluster
-import_resource "aws_iam_role" "tf_eks_cluster_role" "$CLUSTER_ROLE_NAME"
+terraform import aws_iam_role.tf_eks_cluster_role "$CLUSTER_ROLE_NAME" | true
 
 # Importa os policy attachment da role do cluster
-import_resource "aws_iam_role_policy_attachment" "tf_cluster_policy_eks" \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${CLUSTER_POLICY_NAME_1}"
+terraform import aws_iam_role_policy_attachment.tf_cluster_policy_eks \
+"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${CLUSTER_POLICY_NAME_1}" | true
 
-import_resource "aws_iam_role_policy_attachment" "tf_cluster_policy_rds" \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${CLUSTER_POLICY_NAME_2}"
+terraform import aws_iam_role_policy_attachment.tf_cluster_policy_rds \
+"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${CLUSTER_POLICY_NAME_2}" | true
 
-import_resource "aws_iam_role_policy_attachment" "tf_cluster_policy_cloud_watch" \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${CLUSTER_POLICY_NAME_3}"
+terraform import aws_iam_role_policy_attachment.tf_cluster_policy_cloud_watch \
+"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${CLUSTER_POLICY_NAME_3}" | true
 
-import_resource "aws_iam_role_policy_attachment" "tf_cluster_policy_ec2" \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${CLUSTER_POLICY_NAME_4}"
+terraform import aws_iam_role_policy_attachment.tf_cluster_policy_ec2 \
+"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${CLUSTER_POLICY_NAME_4}" | true
 
 # Importa a role do node group
-import_resource "aws_iam_role" "tf_eks_node_group_role" "$NG_ROLE_NAME"
+terraform import aws_iam_role.tf_eks_node_group_role "$NG_ROLE_NAME" | true
 
 # Importa os policy attachment da role do node group
-import_resource "aws_iam_role_policy_attachment" "tf_ng_policy_eks" \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${NG_POLICY_NAME_1}"
+terraform import aws_iam_role_policy_attachment.tf_ng_policy_eks \
+"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${NG_POLICY_NAME_1}" | true
 
-import_resource "aws_iam_role_policy_attachment" "tf_ng_policy_rds" \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${NG_POLICY_NAME_2}"
+terraform import aws_iam_role_policy_attachment.tf_ng_policy_rds \
+"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${NG_POLICY_NAME_2}" | true
 
-import_resource "aws_iam_role_policy_attachment" "tf_ng_policy_eks_cni" \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${NG_POLICY_NAME_3}"
+terraform import aws_iam_role_policy_attachment.tf_ng_policy_eks_cni \
+"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${NG_POLICY_NAME_3}" | true
 
-import_resource "aws_iam_role_policy_attachment" "tf_ng_policy_ec2" \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${NG_POLICY_NAME_4}"
+terraform import aws_iam_role_policy_attachment.tf_ng_policy_ec2 \
+"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${NG_POLICY_NAME_4}" | true
