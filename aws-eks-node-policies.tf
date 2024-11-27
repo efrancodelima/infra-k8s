e1ada3064b547feb91fd229d9984ece53f7341cc
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2024-11-26 22:20:42 -0300

    Correção nas policies do node group

--- aws-eks-node-policies.tf
+++ aws-eks-node-policies.tf
@@ -1,19 +1,19 @@
-resource "aws_iam_role_policy_attachment" "tf_ng_policy_eks" {
+resource "aws_iam_role_policy_attachment" "tf_ng_policy_1" {
   role       = aws_iam_role.tf_eks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "tf_ng_policy_rds" {
+resource "aws_iam_role_policy_attachment" "tf_ng_policy_2" {
   role       = aws_iam_role.tf_eks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
 }
 
-resource "aws_iam_role_policy_attachment" "tf_ng_policy_eks_cni" {
+resource "aws_iam_role_policy_attachment" "tf_ng_policy_3" {
   role       = aws_iam_role.tf_eks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "tf_ng_policy_ec2" {
+resource "aws_iam_role_policy_attachment" "tf_ng_policy_4" {
   role       = aws_iam_role.tf_eks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
