d604168daa7614136009b279ff7c3c5cb0a4896e
efrancodelima <efrancodelima@gmail.com>
2024-11-26 22:03:08 -0300

    Update resource_import.sh

--- resource_import.sh
+++ resource_import.sh
@@ -6,54 +6,54 @@ EKS_NODE_GROUP_NAME="lanchonete-eks-node-group"
 SECURITY_GROUP_ID="sg-07766e32f9a1be753"
 
 CLUSTER_ROLE_NAME="lanchonete-eks-cluster-role"
-CLUSTER_POLICY_NAME_1="AmazonEKSClusterPolicy"
-CLUSTER_POLICY_NAME_2="AmazonRDSFullAccess"
-CLUSTER_POLICY_NAME_3="CloudWatchFullAccess"
-CLUSTER_POLICY_NAME_4="AmazonEC2FullAccess"
+CLUSTER_POLICIES=(
+    "AmazonEKSClusterPolicy"
+    "AmazonRDSFullAccess"
+    "CloudWatchFullAccess"
+    "AmazonEC2FullAccess"
+)
 
 NG_ROLE_NAME="lanchonete-eks-node-group-role"
-NG_POLICY_NAME_1="AmazonEKSWorkerNodePolicy"
-NG_POLICY_NAME_2="AmazonRDSFullAccess"
-NG_POLICY_NAME_3="AmazonEKS_CNI_Policy"
-NG_POLICY_NAME_4="AmazonEC2ContainerRegistryReadOnly"
+NG_POLICIES=(
+    "AmazonEKSWorkerNodePolicy"
+    "AmazonRDSFullAccess"
+    "AmazonEKS_CNI_Policy"
+    "AmazonEC2ContainerRegistryReadOnly"
+)
+
+# Função para importar recursos do Terraform
+import_resource() {
+    local resource_type=$1
+    local resource_name=$2
+    local resource_id=$3
+
+    terraform import "${resource_type}.${resource_name}" "${resource_id}" \
+    || echo "Erro ao importar ${resource_id}, continuando..."
+}
 
 # Importa o cluster EKS
-terraform import aws_eks_cluster.tf_eks_cluster "${EKS_CLUSTER_NAME}" | true
+import_resource "aws_eks_cluster" "tf_eks_cluster" "${EKS_CLUSTER_NAME}"
 
 # Importa o node group
-terraform import aws_eks_node_group.tf_eks_node_group "${EKS_CLUSTER_NAME}:${EKS_NODE_GROUP_NAME}" | true
+import_resource "aws_eks_node_group" "tf_eks_node_group" "${EKS_CLUSTER_NAME}:${EKS_NODE_GROUP_NAME}"
 
 # Importa o security group
-terraform import aws_security_group.tf_eks_security_group "${SECURITY_GROUP_ID}" | true
+import_resource "aws_security_group" "tf_eks_security_group" "${SECURITY_GROUP_ID}"
 
 # Importa a role do cluster
-terraform import aws_iam_role.tf_eks_cluster_role "$CLUSTER_ROLE_NAME" | true
+import_resource "aws_iam_role" "tf_eks_cluster_role" "${CLUSTER_ROLE_NAME}"
 
-# Importa os policy attachment da role do cluster
-terraform import aws_iam_role_policy_attachment.tf_cluster_policy_eks \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${CLUSTER_POLICY_NAME_1}" | true
-
-terraform import aws_iam_role_policy_attachment.tf_cluster_policy_rds \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${CLUSTER_POLICY_NAME_2}" | true
-
-terraform import aws_iam_role_policy_attachment.tf_cluster_policy_cloud_watch \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${CLUSTER_POLICY_NAME_3}" | true
-
-terraform import aws_iam_role_policy_attachment.tf_cluster_policy_ec2 \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${CLUSTER_POLICY_NAME_4}" | true
+# Importa os policy attachments da role do cluster
+for policy in "${CLUSTER_POLICIES[@]}"; do
+    import_resource "aws_iam_role_policy_attachment" "tf_cluster_policy_${policy,,}" \
+    "${CLUSTER_ROLE_NAME}/arn:aws:iam::aws:policy/${policy}"
+done
 
 # Importa a role do node group
-terraform import aws_iam_role.tf_eks_node_group_role "$NG_ROLE_NAME" | true
+import_resource "aws_iam_role" "tf_eks_node_group_role" "${NG_ROLE_NAME}"
 
-# Importa os policy attachment da role do node group
-terraform import aws_iam_role_policy_attachment.tf_ng_policy_eks \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${NG_POLICY_NAME_1}" | true
-
-terraform import aws_iam_role_policy_attachment.tf_ng_policy_rds \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${NG_POLICY_NAME_2}" | true
-
-terraform import aws_iam_role_policy_attachment.tf_ng_policy_eks_cni \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${NG_POLICY_NAME_3}" | true
-
-terraform import aws_iam_role_policy_attachment.tf_ng_policy_ec2 \
-"lanchonete-eks-cluster-role/arn:aws:iam::aws:policy/${NG_POLICY_NAME_4}" | true
+# Importa os policy attachments da role do node group
+for policy in "${NG_POLICIES[@]}"; do
+    import_resource "aws_iam_role_policy_attachment" "tf_ng_policy_${policy,,}" \
+    "${NG_ROLE_NAME}/arn:aws:iam::aws:policy/${policy}"
+done
