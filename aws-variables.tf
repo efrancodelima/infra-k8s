37de67742ac455fc1235880247666b425eeca7c0
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2024-12-08 23:27:31 -0300

    Atualização dos ids da VPC e das subnets

--- aws-variables.tf
+++ aws-variables.tf
@@ -16,19 +16,20 @@ variable "aws_zone_2" {
   default = "us-east-1b"
 }
 
+# VPC e subnets
 variable "vpc_id" {
   description = "O id da VPC"
   type = string
-  default = "vpc-0011200375d4dffa2"
+  default = "vpc-09d12ffbeaeefd374"
 }
 
 variable "subnet_ids" {
-  description = "A lista com os ids das subnets públicas e privadas"
+  description = "A lista com os ids das subnets privadas"
   type = list(string)
   default = [
-    "subnet-074acabffa0581567",   # private 1
-    "subnet-0bba13eebef880c40",   # private 2
-    "subnet-06cf9016ff145c687",   # public 1
-    "subnet-02e71053afdf13d52"    # public 2
+    "subnet-0521cf6f04ef8a9e0",   # public 1
+    "subnet-037f9395a0939a6d0",   # public 2
+    "subnet-0421366e9d4a3fe52",   # private 1
+    "subnet-05cf58f58b3f06a4c"    # private 2
   ]
 }
